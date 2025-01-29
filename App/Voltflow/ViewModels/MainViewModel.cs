43749eae27fecc087d5085ce32f78015cacff576
Hubert <40564309+Qzername@users.noreply.github.com>
2025-01-29 23:02:37 +0100

    Move map to MVVM

--- App/Voltflow/ViewModels/MainViewModel.cs
+++ App/Voltflow/ViewModels/MainViewModel.cs
@@ -1,6 +1,55 @@
-﻿namespace Voltflow.ViewModels;
+﻿using Mapsui.Projections;
+using Mapsui;
+using ReactiveUI;
+using Mapsui.Extensions;
+using System.Diagnostics;
+using Mapsui.Layers;
+using Mapsui.UI.Avalonia;
+using System.Collections.Generic;
+using System;
+
+namespace Voltflow.ViewModels;
 
 public class MainViewModel : ViewModelBase
 {
-    public string Greeting => "Welcome to Avalonia!";
+    private Map _map;
+
+    public Map Map
+    {
+        get => _map;
+        set => this.RaiseAndSetIfChanged(ref _map, value);
+    }
+    
+    MemoryLayer _pointsLayer = new()
+    {
+        Name = "Points",
+        Features = new List<IFeature>(),
+        IsMapInfoLayer = true,
+    };
+
+    public MainViewModel()
+    {
+        Debug.WriteLine("test");
+    }
+
+    public void ConfigureMap()
+    {
+        Map = new Map();
+
+        Map.Layers.Add(Mapsui.Tiling.OpenStreetMap.CreateTileLayer());
+
+        var random = new Random();
+        var lng = random.NextDouble() * 360 - 180; // Random longitude between -180 y 180
+        var lat = random.NextDouble() * 180 - 90;  // Random latutide between -90 y 90
+
+        var feature = new PointFeature(
+            SphericalMercator.FromLonLat(lng, lat).ToMPoint()
+            );
+        feature["name"] = "taaaak";
+
+        ((List<IFeature>)_pointsLayer.Features).Add(feature);
+        Map.RefreshGraphics();
+
+        Map.Layers.Add(_pointsLayer);
+    }
 }
--- App/Voltflow/Views/MainView.axaml.cs
+++ App/Voltflow/Views/MainView.axaml.cs
@@ -1,67 +1,30 @@
 ﻿using Avalonia.Controls;
 using Mapsui;
-using Mapsui.Extensions;
-using Mapsui.Layers;
-using Mapsui.Projections;
-using Mapsui.Styles;
-using Mapsui.UI.Avalonia;
 using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using System.Reflection;
 
 namespace Voltflow.Views;
 
 public partial class MainView : UserControl
 {
-    MemoryLayer _pointsLayer = new()
-    {
-        Name = "Points",
-        Features = new List<IFeature>(),
-        IsMapInfoLayer = true,
-    };
-
-    MapControl mapControl;
-
     public MainView()
     {
         InitializeComponent();
 
-        mapControl = new MapControl();
-
-        Content = mapControl;
-
-        mapControl.Map?.Layers.Add(Mapsui.Tiling.OpenStreetMap.CreateTileLayer());
-        
-        var random = new Random();
-        var lng = random.NextDouble() * 360 - 180; // Random longitude between -180 y 180
-        var lat = random.NextDouble() * 180 - 90;  // Random latutide between -90 y 90
-
-        var feature = new PointFeature(
-            SphericalMercator.FromLonLat(lng, lat).ToMPoint()
-            );
-
-        feature["name"] = "taaaak";
-
-        ((List<IFeature>)_pointsLayer.Features).Add(feature);
-        mapControl?.Map!.RefreshGraphics();
-        
-        if (mapControl?.Map != null)
-            mapControl.Map.Info += OnChangeMapInfo;
-
-        mapControl.Map?.Layers.Add(_pointsLayer);
+        /*
+         * View's constructor is called faster than viewmodel constructor
+         * therefore i need to wait for the datacontext change and call map configuration
+         * 
+         * I as well cannot bind map directly to the viewmodel
+         * because this library for some reason does not support this 
+         */
+        DataContextChanged += MainView_DataContextChanged;
     }
-
-    void OnChangeMapInfo(object? sender, MapInfoEventArgs e)
+    
+    private void MainView_DataContextChanged(object? sender, EventArgs e)
     {
-        if (e.MapInfo?.Feature != null)
-        {
-            var clickedFeature = e.MapInfo.Feature;
-            var name = clickedFeature["name"]?.ToString();
+        var viewModel = DataContext as ViewModels.MainViewModel;
+        viewModel!.ConfigureMap();
 
-            Console.WriteLine(name);
-        }
+        map.Map = viewModel.Map;
     }
-
 }
