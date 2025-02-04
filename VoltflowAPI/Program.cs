7d062be1d69c166896c7a5a05507e3242022bfe4
Qzername <40564309+Qzername@users.noreply.github.com>
2025-02-04 14:05:49 +0100

    Add template and clean program.cs

--- VoltflowAPI/Program.cs
+++ VoltflowAPI/Program.cs
@@ -1,8 +1,21 @@
+/*
+ * Before your first run:
+ * You will have to modify appsettings in order to run the server
+ * 
+ * Required elements are:
+ * - ConnectionStrings -> Default
+ * - Jwt -> Key
+ * 
+ * Check the appsettings.template.json for example
+ */
+
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+
 using System.Text;
+
 using VoltflowAPI.Contexts;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,14 +23,22 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 
-string connectionString = builder.Configuration.GetConnectionString("Default")!;
+// --- Contexts ---
 
+// --- Dependency Injection ---
+
+// --- PostgreSQL Server configuration ---
+//Requires ConnectionStrings -> Default to be set 
+string connectionString = builder.Configuration.GetConnectionString("Default")!;
 builder.Services.AddDbContext<ApplicationContext>(opt => opt.UseNpgsql(connectionString));
 
+// --- ASP.NET Identity configuration --- 
 builder.Services.AddIdentity<Account, IdentityRole>()
     .AddEntityFrameworkStores<ApplicationContext>()
     .AddDefaultTokenProviders();
 
+// --- Authentication and autorization --- 
+//Requires Jwt -> Key to be set
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddJwtBearer(o =>
     {
@@ -26,13 +47,15 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
             IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!)),
             ValidateLifetime = true,
             ValidateIssuerSigningKey = true,
+            //for now, we don't have domain
+            //this will require configuration when server is bought
             ValidateAudience = false,
             ValidateIssuer = false,
         };
     });
-
 builder.Services.AddAuthorization();
 
+// --- App conifg ---
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
