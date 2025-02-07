40485d277869521a3b76e749634a90a86d44350e
Hubert <40564309+Qzername@users.noreply.github.com>
2025-02-07 20:25:11 +0100

    Add view locators

---
+++ App/Voltflow/ViewLocators/AvaloniaViewLocator.cs
@@ -0,0 +1,36 @@
+﻿using Avalonia.Controls.Templates;
+using Avalonia.Controls;
+using System;
+using Voltflow.ViewModels;
+using ReactiveUI;
+
+namespace Voltflow.ViewLocators;
+
+/// <summary>
+/// This viewlocator tells Avalonia which view to use for a given viewmodel.
+/// 
+/// if i set the datacontext of a control to a viewmodel,
+/// then this viewlocator will find the view for that viewmodel.
+/// </summary>
+public class AvaloniaViewLocator : IDataTemplate
+{
+    public Control Build(object data)
+    {
+        var name = data.GetType().FullName!.Replace("ViewModel", "View");
+        var type = Type.GetType(name);
+
+        if (type != null)
+        {
+            return (Control)Activator.CreateInstance(type)!;
+        }
+        else
+        {
+            return new TextBlock { Text = "Not Found: " + name };
+        }
+    }
+
+    public bool Match(object data)
+    {
+        return data is ViewModelBase;
+    }
+}
---
+++ App/Voltflow/ViewLocators/ReactiveViewLocator.cs
@@ -0,0 +1,22 @@
+﻿using Avalonia.Controls;
+using ReactiveUI;
+using System;
+
+namespace Voltflow.ViewLocators;
+
+/// <summary>
+///  This viewlocator tells ReactiveUI library which view to use for a given viewmodel.
+///  
+/// if i set the datacontext of a control to a viewmodel,
+/// then this viewlocator will find the view for that viewmodel.
+/// </summary>
+public class ReactiveViewLocator : IViewLocator
+{
+    IViewFor IViewLocator.ResolveView<T>(T viewModel, string contract)
+    {
+        var name = viewModel!.GetType().FullName!.Replace("ViewModel", "View");
+        var type = Type.GetType(name);
+
+        return (IViewFor)Activator.CreateInstance(type!)!;
+    }
+}
