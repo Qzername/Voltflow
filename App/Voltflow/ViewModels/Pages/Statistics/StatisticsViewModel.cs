d1c56bb2e50b2b8086deb8c102ddee4189730ddd
heapy <63428380+heapyxyz@users.noreply.github.com>
2025-02-19 14:42:11 +0100

    Add Statistics page (empty)

---
+++ App/Voltflow/ViewModels/Pages/Statistics/StatisticsViewModel.cs
@@ -0,0 +1,9 @@
+﻿using ReactiveUI;
+using Voltflow.ViewModels.Pages.Map;
+
+namespace Voltflow.ViewModels.Pages.Statistics;
+
+public class StatisticsViewModel(IScreen screen) : ViewModelBase(screen)
+{
+	public void NavigateHome() => HostScreen.Router.NavigateAndReset.Execute(new MapViewModel(screen));
+}
---
+++ App/Voltflow/Views/Pages/Statistics/StatisticsView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.ReactiveUI;
+using Voltflow.ViewModels.Pages.Statistics;
+
+namespace Voltflow.Views.Pages.Statistics;
+
+public partial class StatisticsView : ReactiveUserControl<StatisticsViewModel>
+{
+    public StatisticsView()
+    {
+        InitializeComponent();
+    }
+}
