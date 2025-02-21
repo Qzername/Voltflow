8e9c03f70c568509f7a34a9370383b7a60bfbd1d
Hubert <40564309+Qzername@users.noreply.github.com>
2025-02-21 21:44:00 +0100

    Add test pie chart

--- App/Voltflow/ViewModels/Pages/Statistics/StatisticsViewModel.cs
+++ App/Voltflow/ViewModels/Pages/Statistics/StatisticsViewModel.cs
@@ -1,9 +1,36 @@
-﻿using ReactiveUI;
+﻿using LiveChartsCore.SkiaSharpView.Drawing.Geometries;
+using LiveChartsCore.SkiaSharpView;
+using LiveChartsCore;
+using ReactiveUI;
 using Voltflow.ViewModels.Pages.Map;
+using LiveChartsCore.SkiaSharpView.Extensions;
+using LiveChartsCore.SkiaSharpView.VisualElements;
+using System.Collections.Generic;
 
 namespace Voltflow.ViewModels.Pages.Statistics;
 
 public class StatisticsViewModel(IScreen screen) : ViewModelBase(screen)
 {
-	public void NavigateHome() => HostScreen.Router.NavigateAndReset.Execute(new MapViewModel(HostScreen));
+    public IEnumerable<ISeries> Series { get; set; } =
+        new[] { 2, 4, 1, 4, 3 }.AsPieSeries();
+
+    // the expression above is equivalent to the next series collection:
+    public IEnumerable<ISeries> Series2 { get; set; } =
+        [
+            new PieSeries<int> { Values = [2] },
+            new PieSeries<int> { Values = [4] },
+            new PieSeries<int> { Values = [1] },
+            new PieSeries<int> { Values = [4] },
+            new PieSeries<int> { Values = [3] },
+        ];
+
+    public LabelVisual Title { get; set; } =
+        new LabelVisual
+        {
+            Text = "My chart title",
+            TextSize = 25,
+            Padding = new LiveChartsCore.Drawing.Padding(15)
+        };
+
+    public void NavigateHome() => HostScreen.Router.NavigateAndReset.Execute(new MapViewModel(HostScreen));
 }
