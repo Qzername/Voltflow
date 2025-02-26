14284eadce29280d0187a19f609c29a90a932192
heapy <63428380+heapyxyz@users.noreply.github.com>
2025-02-26 23:54:00 +0100

    Finish account's settings

---
+++ App/Voltflow/Models/Forms/SettingsForm.cs
@@ -0,0 +1,26 @@
+﻿using ReactiveUI.Fody.Helpers;
+
+namespace Voltflow.Models.Forms;
+
+/// <summary>
+/// Represents the settings form.
+/// </summary>
+public class SettingsForm : BaseForm
+{
+	[Reactive] public bool TwoFactor { get; set; }
+	[Reactive] public string? Name { get; set; }
+	[Reactive] public string? Surname { get; set; }
+	[Reactive] public string? PhoneNumber { get; set; }
+
+	/// <summary>
+	/// Resets variables stored in the form.
+	/// </summary>
+	public void Reset()
+	{
+		TwoFactor = false;
+		Name = null;
+		Surname = null;
+		PhoneNumber = null;
+		Working = false;
+	}
+}
--- App/Voltflow/ViewModels/Account/SettingsViewModel.cs
+++ App/Voltflow/ViewModels/Account/SettingsViewModel.cs
@@ -1,8 +1,111 @@
-﻿using ReactiveUI;
+﻿using System.Diagnostics;
+using Avalonia.Controls.Notifications;
+using Avalonia.SimplePreferences;
+using ReactiveUI;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Ursa.Controls;
+using Voltflow.Models;
+using Voltflow.Models.Forms;
+using Voltflow.ViewModels.Pages.Map;
 
 namespace Voltflow.ViewModels.Account;
 
-public class SettingsViewModel(IScreen screen) : ViewModelBase(screen)
+public class SettingsViewModel : ViewModelBase
 {
+	public WindowToastManager? ToastManager;
+	public SettingsForm SettingsForm { get; set; }
 
+	private readonly HttpClient _httpClient;
+	private bool _clickedDelete;
+
+	public SettingsViewModel(IScreen screen, SettingsForm settingsForm) : base(screen)
+	{
+		_httpClient = GetService<HttpClient>();
+		SettingsForm = settingsForm;
+	}
+
+	public async Task SaveChanges()
+	{
+		bool nameValid = SettingsForm.Name == null || NameValidator.IsValid(SettingsForm.Name);
+		bool surnameValid = SettingsForm.Surname == null || NameValidator.IsValid(SettingsForm.Surname);
+		bool phoneNumberValid = SettingsForm.PhoneNumber == null || NumberValidator.IsValid(SettingsForm.PhoneNumber, 9);
+
+		if (!nameValid || !surnameValid || !phoneNumberValid)
+		{
+			ToastManager?.Show(
+				new Toast("Provided credentials do not meet our rules!"),
+				showIcon: true,
+				showClose: false,
+				type: NotificationType.Error,
+				classes: ["Light"]);
+			return;
+		}
+
+		SettingsForm.Working = true;
+
+		var json = new
+		{
+			Name = SettingsForm.Name,
+			Surname = SettingsForm.Surname,
+			PhoneNumber = SettingsForm.PhoneNumber
+		};
+
+		var content = JsonConverter.ToStringContent(json);
+		var request = await _httpClient.PatchAsync("/api/Accounts", content);
+		var twoFactorRequest = await _httpClient.PostAsync($"/api/Identity/TwoFactor/{(SettingsForm.TwoFactor ? "enable" : "disable")}", null);
+
+		if (request.StatusCode == HttpStatusCode.OK && twoFactorRequest.StatusCode == HttpStatusCode.OK)
+			HostScreen.Router.NavigateAndReset.Execute(new AccountViewModel(HostScreen));
+		else
+			ToastManager?.Show(
+				new Toast("Couldn't save changes."),
+				showIcon: true,
+				showClose: false,
+				type: NotificationType.Error,
+				classes: ["Light"]);
+
+		SettingsForm.Working = false;
+	}
+
+	public async Task DeleteAccount()
+	{
+		if (!_clickedDelete)
+		{
+			_clickedDelete = true;
+			ToastManager?.Show(
+				new Toast("Are you sure? If yes, press \"Delete Account\" again."),
+				showIcon: true,
+				showClose: false,
+				type: NotificationType.Warning,
+				classes: ["Light"]);
+
+			return;
+		}
+
+		SettingsForm.Working = true;
+		var request = await _httpClient.DeleteAsync("/api/Accounts");
+
+		if (request.StatusCode != HttpStatusCode.OK)
+		{
+			_clickedDelete = false; // In case user changed their mind
+			ToastManager?.Show(
+				new Toast("Couldn't delete account."),
+				showIcon: true,
+				showClose: false,
+				type: NotificationType.Error,
+				classes: ["Light"]);
+
+			return;
+		}
+
+		await Preferences.RemoveAsync("token");
+		_httpClient.DefaultRequestHeaders.Authorization = null;
+
+		if (HostScreen is MainViewModel screen)
+			screen.Authenticated = false;
+
+		HostScreen.Router.NavigateAndReset.Execute(new MapViewModel(HostScreen));
+	}
 }
