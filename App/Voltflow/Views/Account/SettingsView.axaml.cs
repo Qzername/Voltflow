41a1c0fd03913f87fbb0ab8631dc34c61eae966f
heapy <63428380+heapyxyz@users.noreply.github.com>
2025-02-26 23:54:00 +0100

    Finish account's settings

--- App/Voltflow/Views/Account/SettingsView.axaml.cs
+++ App/Voltflow/Views/Account/SettingsView.axaml.cs
@@ -1,4 +1,7 @@
+using Avalonia;
+using Avalonia.Controls;
 using Avalonia.ReactiveUI;
+using Ursa.Controls;
 using Voltflow.ViewModels.Account;
 
 namespace Voltflow.Views.Account;
@@ -8,5 +11,24 @@ public partial class SettingsView : ReactiveUserControl<SettingsViewModel>
 	public SettingsView()
 	{
 		InitializeComponent();
+	}
+
+	// https://github.com/irihitech/Ursa.Avalonia/blob/main/demo/Ursa.Demo/Pages/ToastDemo.axaml.cs
+	protected override void OnAttachedToVisualTree(VisualTreeAttachmentEventArgs e)
+	{
+		base.OnAttachedToVisualTree(e);
+
+		if (DataContext is not SettingsViewModel viewModel)
+			return;
+
+		var topLevel = TopLevel.GetTopLevel(this);
+		viewModel.ToastManager = new WindowToastManager(topLevel) { MaxItems = 1 };
+	}
+
+	protected override void OnDetachedFromVisualTree(VisualTreeAttachmentEventArgs e)
+	{
+		base.OnDetachedFromVisualTree(e);
+		if (DataContext is SettingsViewModel viewModel)
+			viewModel.ToastManager?.Uninstall();
 	}
 }
