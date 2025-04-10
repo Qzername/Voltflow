e7d7d7dc7f39b71bca963ab3f685389de8e089b9
heapy <63428380+heapyxyz@users.noreply.github.com>
2025-04-10 23:18:24 +0200

    aaaaaa

--- ChargingStation/main.py
+++ ChargingStation/main.py
@@ -4,6 +4,7 @@ import server_connection
 import display
 import tkinter as tk
 import time
+import charging_status
 
 import configparser
 
@@ -35,7 +36,11 @@ def loop():
     server_connection.get_port(0)
     server_connection.get_port(1)
 
-    print(server_connection.ports[0])    
+    print(server_connection.ports)
+
+    wattages = charging_status.get_info()
+    server_connection.set_wattage(0, wattages[0])
+    server_connection.set_wattage(1, wattages[1])
 
     root.after(1000, loop)
 
