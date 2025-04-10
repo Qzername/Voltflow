f5ec98845ee7aab267041454ff2e1f031f42e81e
heapy <63428380+heapyxyz@users.noreply.github.com>
2025-04-10 18:39:30 +0200

    Update PiChargingHub.cs

--- VoltflowAPI/Hubs/PiChargingHub.cs
+++ VoltflowAPI/Hubs/PiChargingHub.cs
@@ -7,10 +7,18 @@ namespace VoltflowAPI.Hubs;
 [AllowAnonymous]
 public class PiChargingHub : Hub
 {
+    private bool _enabled;
+
     public override async Task OnConnectedAsync()
     {
         Debug.WriteLine("Connection opened");
-        await Clients.Caller.SendAsync("TurnPortOff", 0);
+
+        while (true)
+        {
+            await Task.Delay(1000);
+            await Clients.Caller.SendAsync(_enabled ? "TurnPortOff" : "TurnPortOn", 0);
+            _enabled = !_enabled;
+        }
     }
 
     public override async Task OnDisconnectedAsync(Exception exception)
