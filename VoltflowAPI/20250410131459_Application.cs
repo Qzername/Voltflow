d028f8157081b5d5346375ac1b9c55fa30f21e13
Qzername <40564309+Qzername@users.noreply.github.com>
2025-04-10 16:36:46 +0200

    Add migrations

---
+++ VoltflowAPI/20250410131459_Application.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace VoltflowAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class Application : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cars",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    AccountId = table.Column<string>(type: "text", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    BatteryCapacity = table.Column<int>(type: "integer", nullable: false),
+                    ChargingRate = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cars", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ChargingStations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Longitude = table.Column<double>(type: "double precision", nullable: false),
+                    Latitude = table.Column<double>(type: "double precision", nullable: false),
+                    Cost = table.Column<int>(type: "integer", nullable: false),
+                    MaxChargeRate = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ChargingStations", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ChargingStationServiceHistory",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    StationId = table.Column<int>(type: "integer", nullable: false),
+                    EndDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ChargingStationServiceHistory", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Transactions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    AccountId = table.Column<string>(type: "text", nullable: false),
+                    CarId = table.Column<int>(type: "integer", nullable: true),
+                    ChargingStationId = table.Column<int>(type: "integer", nullable: false),
+                    StartDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    EndDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    EnergyConsumed = table.Column<double>(type: "double precision", nullable: false),
+                    Cost = table.Column<double>(type: "double precision", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Transactions", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ChargingPorts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    StationId = table.Column<int>(type: "integer", nullable: false),
+                    Status = table.Column<int>(type: "integer", nullable: false),
+                    ServiceMode = table.Column<bool>(type: "boolean", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ChargingPorts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ChargingPorts_ChargingStations_StationId",
+                        column: x => x.StationId,
+                        principalTable: "ChargingStations",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ChargingStationOpeningHours",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    StationId = table.Column<int>(type: "integer", nullable: false),
+                    Monday = table.Column<TimeSpan[]>(type: "interval[]", nullable: false),
+                    Tuesday = table.Column<TimeSpan[]>(type: "interval[]", nullable: false),
+                    Wednesday = table.Column<TimeSpan[]>(type: "interval[]", nullable: false),
+                    Thursday = table.Column<TimeSpan[]>(type: "interval[]", nullable: false),
+                    Friday = table.Column<TimeSpan[]>(type: "interval[]", nullable: false),
+                    Saturday = table.Column<TimeSpan[]>(type: "interval[]", nullable: false),
+                    Sunday = table.Column<TimeSpan[]>(type: "interval[]", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ChargingStationOpeningHours", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ChargingStationOpeningHours_ChargingStations_StationId",
+                        column: x => x.StationId,
+                        principalTable: "ChargingStations",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ChargingPorts_StationId",
+                table: "ChargingPorts",
+                column: "StationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ChargingStationOpeningHours_StationId",
+                table: "ChargingStationOpeningHours",
+                column: "StationId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cars");
+
+            migrationBuilder.DropTable(
+                name: "ChargingPorts");
+
+            migrationBuilder.DropTable(
+                name: "ChargingStationOpeningHours");
+
+            migrationBuilder.DropTable(
+                name: "ChargingStationServiceHistory");
+
+            migrationBuilder.DropTable(
+                name: "Transactions");
+
+            migrationBuilder.DropTable(
+                name: "ChargingStations");
+        }
+    }
+}
