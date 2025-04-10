45e4af37cb69926f3e7121fda1878e477dd76e49
Qzername <40564309+Qzername@users.noreply.github.com>
2025-04-10 18:42:22 +0200

    Update display.py

--- ChargingStation/display.py
+++ ChargingStation/display.py
@@ -2,6 +2,12 @@ from main import Matrix
 
 matrix = Matrix()
 
+while matrix.ready():
+        matrix.reset(matrix.color("red"))
+        matrix.show()
+        matrix.reset(matrix.color("blue"))
+        matrix.show()
+
 def test_matrix():
     matrix.reset()  # black background (0, 0, 0)
     matrix.line((0, 0), (60, 30), (255, 0, 0), 1)  # diagonal red line
@@ -9,5 +15,3 @@ def test_matrix():
     matrix.circle((30, 15), 10, (0, 0, 255), 2)  # blue circle
     matrix.show()
     matrix.delay(10000) 
-
-test_matrix()
