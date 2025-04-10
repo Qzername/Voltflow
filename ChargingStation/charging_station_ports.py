8651f3c8616bce08803f84129dc787c966b589a2
Qzername <40564309+Qzername@users.noreply.github.com>
2025-04-10 17:48:00 +0200

    Working ports

--- ChargingStation/charging_station_ports.py
+++ ChargingStation/charging_station_ports.py
@@ -1,30 +1,19 @@
 import RPi.GPIO as GPIO
-import time
 
 RELAY_LEFT_PIN = 14
 RELAY_RIGHT_PIN = 15
 
-# Set the GPIO mode
-GPIO.setmode(GPIO.BCM)  # Use BCM numbering (GPIO numbers)
+pin_data = {
+    0: RELAY_LEFT_PIN,
+    1: RELAY_RIGHT_PIN
+}
 
 # Set up the GPIO pin
 GPIO.setup(RELAY_LEFT_PIN, GPIO.OUT)
-
-# Turn relay ON
-GPIO.output(RELAY_LEFT_PIN, GPIO.LOW)  # Some relays are active-low
-print("Relay ON")
-time.sleep(2)
-
-# Turn relay OFF
-GPIO.output(RELAY_LEFT_PIN, GPIO.HIGH)
-print("Relay OFF")
-time.sleep(2)
-
-# Cleanup
-GPIO.cleanup()
+GPIO.setup(RELAY_RIGHT_PIN, GPIO.OUT)
 
 def turn_port_on(port):
-    pass
+    GPIO.output(pin_data[port], GPIO.HIGH)
 
 def turn_port_off(port):
-    pass
+    GPIO.output(pin_data[port], GPIO.LOW) 
