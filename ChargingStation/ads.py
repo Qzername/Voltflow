c033e0dbeb84129b1832dde2bdce3ddfa340facc
heapy <63428380+heapyxyz@users.noreply.github.com>
2025-04-11 06:02:44 +0200

    hmmmm

--- ChargingStation/ads.py
+++ ChargingStation/ads.py
@@ -21,12 +21,14 @@ def config(frame, rootMain):
     global right_frame
 
     right_frame = tk.Frame(frame)
+    right_frame.grid_rowconfigure(0, weight=1)
+    right_frame.grid_columnconfigure(0, weight=1)
     right_frame.pack(side=tk.RIGHT, padx=10, pady=10)
 
     global image_label
 
     image_label = tk.Label(right_frame)
-    image_label.pack(side=tk.TOP)
+    image_label.grid(row=0, column=0)
     
     update_image()  # start rotacji obrazków
 
