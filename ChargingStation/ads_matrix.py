e276ed985b18dd84bb205cc69f5423ae5a0dab90
Qzername <40564309+Qzername@users.noreply.github.com>
2025-04-11 07:35:24 +0200

    Update ads_matrix.py

--- ChargingStation/ads_matrix.py
+++ ChargingStation/ads_matrix.py
@@ -38,8 +38,6 @@ def update_matrix():
         for x in range(width):
             pixel = image.getpixel((x, y))
 
-            print((pixel[0], pixel[1],pixel[2]))
-
             display.set_pixel(width-x-1,y,(pixel[0], pixel[1],pixel[2]))
 
     display.apply_changes()
