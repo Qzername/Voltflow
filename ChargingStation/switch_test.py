3746d81da3b2a02f2427c7df382b885de3ee63f9
Qzername <40564309+Qzername@users.noreply.github.com>
2025-04-11 02:50:17 +0200

    Update switch_test.py

--- ChargingStation/switch_test.py
+++ ChargingStation/switch_test.py
@@ -6,18 +6,12 @@ SWITCH_PIN = 26  # Use BCM numbering
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(SWITCH_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # Pull-up
 
-def switch_callback(channel):
-    if GPIO.input(channel) == GPIO.LOW:
-        print("Switch pressed")
-    else:
-        print("Switch released")
-
-# Add interrupt detection
-GPIO.remove_event_detect(SWITCH_PIN)
-GPIO.add_event_detect(SWITCH_PIN, GPIO.BOTH, callback=switch_callback, bouncetime=200)
-
 try:
     while True:
+        if GPIO.input(SWITCH_PIN) == GPIO.LOW:
+            print("Switch pressed")
+        else:
+            print("Switch released")
         time.sleep(1)
 except KeyboardInterrupt:
     GPIO.cleanup()
